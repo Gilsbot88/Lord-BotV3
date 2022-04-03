e9e31085fd7736f667fa51b0dcb4214924b8e8b3
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-04-04 05:32:48 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `››╭─〘 *Donasi* 〙
 ╭╡
-│┝‷✧ *Pulsa:* 6287708773367
-│┝‷✧ *Dana:* 6285712420674
-│┝‷✧ *Linkaja:* 6285712420674
-│┝‷✧ *Gopay:* 6285712420674
-│┝‷✧ *Ovo:* 6285712420674
+│┝‷✧ *Pulsa:* 6283136890848
+│┝‷✧ *Dana:* 6283136890848
+│┝‷✧ *Linkaja:* belum ada
+│┝‷✧ *Gopay:* pc owner
+│┝‷✧ *Ovo:* belum ada
 │╰───···─────
 │⁺◛˖ Ingin Donasi? Chat
-│wa.me/6288229683561?text=kak+mau+donasi
+│wa.me/6283136890848?text=kak+mau+donasi
 ╰──────────···───╮
 ▌│█║▌║▌║║▌║▌║█│▌
 `.trim(), `${wm}`, 'Owner', '.owner')
